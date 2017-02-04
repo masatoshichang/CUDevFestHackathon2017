2f2bd7cd99aee5ea16b4bceabc0cdb3a81b46691
Masatoshi Chang <masatoshi.chang@gmail.com>
2017-02-04 03:03:42 -0500

    Loop through several months

--- run.py
+++ run.py
@@ -8,28 +8,30 @@ from results import *
 
 if __name__ == '__main__':
     payload = {}
-    search_range = '2016/1'
-    archive_list = get_archive(payload, search_range)
 
-    result_list = []
+    for m in range(2, 13):
+        search_range = '2016/{}'.format(m)
+        archive_list = get_archive(payload, search_range)
 
-    num_articles = len(archive_list)
-    
-    i = 0
-    for an_article in archive_list:
-        i += 1
-        print('{}/{}'.format(i, num_articles))
-        image_url = an_article.image_url
-        emotion_json = get_emotion(image_url)
+        result_list = []
 
-        face_json = get_face(image_url)
+        num_articles = len(archive_list)
+        
+        i = 0
+        for an_article in archive_list:
+            i += 1
+            print('{}/{}'.format(i, num_articles))
+            image_url = an_article.image_url
+            emotion_json = get_emotion(image_url)
 
-        result = Result(an_article, emotion_json, face_json)
-        result_list.append(result)
+            face_json = get_face(image_url)
+
+            result = Result(an_article, emotion_json, face_json)
+            result_list.append(result)
 
 
-        time.sleep(1)
+            time.sleep(1)
 
 
-    
+        
     save_to_pickle(result_list)
