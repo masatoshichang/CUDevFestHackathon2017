609f0a0b003e596953fde3398c7b2a81ca14a061
jacquesva <vajacques@gmail.com>
2017-02-04 03:51:56 -0500

    Process emotion probabilities

--- process_results.py
+++ process_results.py
@@ -1,6 +1,6 @@
 from article_search import *
 from results import *
-
+from collections import defaultdict
 
 
 
@@ -9,6 +9,15 @@ if __name__ == '__main__':
     
     num = 0
     num_faces = 0
+
+    emotions_list = ['sadness', 'neutral', 'contempt', 'disgust', 'anger', 'surprise', 'fear', 'happiness']
+    averages = {}
+    for e in emotions_list:
+        averages[e] = 0
+
+    section_names = defaultdict(int)
+
+
     for a_result in results_list:
         if len(a_result.emotion_result) == 0:
             # No faces?
@@ -43,19 +52,28 @@ if __name__ == '__main__':
             fear = emotion['fear']
             happiness = emotion['happiness']
 
+            for e in emotions_list:
+                averages[e] += emotion[e]
+
             """
             title, date, section, subsection, news_desk, img_url, face_rectangle, sadness, neutral, contempt, disgust, anger, surprise, fear, happiness
             """
 
-            input_values = [title, date, section, subsection, news_desk, img_url, face_rectangle, sadness, neutral, contempt, disgust, anger, surprise, fear, happiness]
-            print(input_values)
+            #input_values = [title, date, section, subsection, news_desk, img_url, face_rectangle, sadness, neutral, contempt, disgust, anger, surprise, fear, happiness]
+            #input_values = [title, date, section, subsection, news_desk, emotion]
+            #print(input_values)
             num_faces += 1
+            section_names[section] += 1
 
-            insert_sql_database(input_values)
+            #insert_sql_database(input_values)
         num += 1
         print(num)
 
+    for e in emotions_list:
+        averages[e] = averages[e]/num_faces
+    print averages
     print('num: {}'.format(num)) 
     print('num_faces: {}'.format(num_faces))
+    print section_names
 
         
