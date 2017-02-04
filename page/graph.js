931201d8e771a49fe2255f9f69ce974f1a3e18be
Luke Zhang <lukezhang213@gmail.com>
2017-02-04 05:20:27 -0500

    Page data connection test

---
+++ page/graph.js
@@ -0,0 +1,17 @@
+var connection = new ActiveXObject("ADODB.Connection") ;
+
+var connectionstring="Driver={ODBC Driver 13 for SQL Server};Server=tcp:cudevfest2017.database.windows.net,1433;Database=cudevfest2017;Uid=devfest@cudevfest2017;Pwd=!23QweAsdZxc;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;";
+
+connection.Open(connectionstring);
+var rs = new ActiveXObject("ADODB.Recordset");
+
+rs.Open("SELECT * FROM dbo.EmotionSections ORDER BY date ASC LIMIT 10", connection);
+rs.MoveFirst()
+while(!rs.eof)
+{
+   console.log(rs.fields(2));
+   rs.movenext();
+}
+
+rs.close();
+connection.close(); 
