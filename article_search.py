b2728a8e4a4ed55f7b498cab9c85b079c18e31dd
Masatoshi Chang <masatoshi.chang@gmail.com>
2017-02-03 21:21:24 -0500

    Properly pass api key

--- article_search.py
+++ article_search.py
@@ -5,11 +5,13 @@ NY_TIMES_API_KEY = '8c6b810edc3a42de829129b0e38eaf99'
 NY_TIMES_ARCHIVE_API = 'https://api.nytimes.com/svc/archive/v1/'
 
 def get_articles(json_data): # only returns 10 articles
+    json_data['api-key'] = NY_TIMES_API_KEY
     r = requests.get(NY_TIMES_ARTICLE_API, params=json_data)
     return r.json()
 
 
 def get_archive(json_data, search_range):
+    json_data['api-key'] = NY_TIMES_API_KEY
     url = NY_TIMES_ARCHIVE_API + search_range + '.json'
     r = requests.get(url, params=json_data)
     return r.json()
@@ -17,7 +19,6 @@ def get_archive(json_data, search_range):
 
 if __name__ == '__main__':
     payload = {
-    'api-key': "8c6b810edc3a42de829129b0e38eaf99",
     'fq': "section_name:(\"Technology\")",
     'begin_date': "20160101",
     'end_date': "20160131"
