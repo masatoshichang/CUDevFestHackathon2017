0ab3bbf6300c4d111a8c25d465b956894abf2551
Masatoshi Chang <masatoshi.chang@gmail.com>
2017-02-04 03:28:58 -0500

    Save to pickle once in a while

--- run.py
+++ run.py
@@ -33,6 +33,9 @@ if __name__ == '__main__':
             result = Result(an_article, emotion_json)
             result_list.append(result)
 
+        if i % 50 == 0:
+            save_to_pickle(result_list)
+
 
         
     save_to_pickle(result_list)
